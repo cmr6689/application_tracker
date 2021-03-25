17b785718595216a9285e082342e56591beaf2cb
cmr6689 <cmr6689@g.rit.edu>
2021-03-25 17:16:57 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,209 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+class Company extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A company was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Company
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+            </form>
+        );
+    }
+}
+
+class Position extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A position was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Position
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+            </form>
+        );
+    }
+}
+
+class DateApplied extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A date was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Date Applied
+                    <input type="date" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+            </form>
+        );
+    }
+}
+
+class Status extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A date was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Status
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+            </form>
+        );
+    }
+}
+
+class Notes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A date was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Notes
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+            </form>
+        );
+    }
+}
+
+class Application extends React.Component {
+    renderPosition() {
+        return (
+            <Position />
+        );
+    }
+
+    renderCompany() {
+        return (
+            <Company />
+        );
+    }
+
+    renderDateApplied() {
+        return (
+            <DateApplied />
+        );
+    }
+
+    renderStatus() {
+        return (
+            <Status />
+        );
+    }
+
+    renderNotes() {
+        return (
+            <Notes />
+        );
+    }
+
+    handleSubmit(event) {
+        alert("Position " + this.renderPosition().value)
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderPosition()}
+                {this.renderCompany()}
+                {this.renderDateApplied()}
+                {this.renderStatus()}
+                {this.renderNotes()}
+                <form onSubmit={this.handleSubmit}>
+                    <input type="submit" value="Add Application" />
+                </form>
+            </div>
+
+        );
+    }
+
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Application />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
